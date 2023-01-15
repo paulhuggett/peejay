a367e3aaa857c58363c750628626bd2f43307e12
Paul Bowen-Huggett <paulhuggett@mac.com>
2023-01-15 10:25:32 +0100

    clang-format.

--- unittests/test_whitespace.cpp
+++ unittests/test_whitespace.cpp
@@ -1,12 +1,13 @@
 #include <gtest/gtest.h>
+
 #include "callbacks.hpp"
 #include "peejay/json.hpp"
 
 using namespace std::string_view_literals;
-using peejay::make_parser;
 using peejay::char_set;
-using peejay::extensions;
 using peejay::error;
+using peejay::extensions;
+using peejay::make_parser;
 
 namespace {
 
@@ -21,7 +22,7 @@ protected:
 // NOLINTNEXTLINE
 TEST_F (Whitespace, Empty) {
   EXPECT_CALL (callbacks_, uint64_value (0)).Times (1);
-  auto p = make_parser(proxy_);
+  auto p = make_parser (proxy_);
   p.input (u8"0"sv).eof ();
   EXPECT_FALSE (p.has_error ());
 }
@@ -29,7 +30,7 @@ TEST_F (Whitespace, Empty) {
 // NOLINTNEXTLINE
 TEST_F (Whitespace, MultipleLeadingSpaces) {
   EXPECT_CALL (callbacks_, uint64_value (0)).Times (1);
-  auto p = make_parser(proxy_);
+  auto p = make_parser (proxy_);
   p.input (u8"    0"sv).eof ();
   EXPECT_FALSE (p.has_error ());
 }
@@ -37,26 +38,31 @@ TEST_F (Whitespace, MultipleLeadingSpaces) {
 // NOLINTNEXTLINE
 TEST_F (Whitespace, MultipleTrailingSpaces) {
   EXPECT_CALL (callbacks_, uint64_value (0)).Times (1);
-  auto p = make_parser(proxy_);
+  auto p = make_parser (proxy_);
   p.input (u8"0    "sv).eof ();
   EXPECT_FALSE (p.has_error ());
 }
 
 namespace {
-  std::u32string const extra_ws_chars {char_set::character_tabulation, char_set::vertical_tabulation, char_set::space, char_set::no_break_space, char_set::en_quad, '0'};
+std::u32string const extra_ws_chars{char_set::character_tabulation,
+                                    char_set::vertical_tabulation,
+                                    char_set::space,
+                                    char_set::no_break_space,
+                                    char_set::en_quad,
+                                    '0'};
 }
 
 // NOLINTNEXTLINE
 TEST_F (Whitespace, ExtendedWhitespaceCharactersEnabled) {
   EXPECT_CALL (callbacks_, uint64_value (0)).Times (1);
-  auto p = make_parser(proxy_, extensions::extra_whitespace);
+  auto p = make_parser (proxy_, extensions::extra_whitespace);
   p.input (extra_ws_chars).eof ();
   EXPECT_FALSE (p.has_error ());
 }
 
 // NOLINTNEXTLINE
 TEST_F (Whitespace, ExtendedWhitespaceCharactersDisabled) {
-  auto p = make_parser(proxy_);
+  auto p = make_parser (proxy_);
   p.input (extra_ws_chars).eof ();
   EXPECT_TRUE (p.has_error ());
   EXPECT_EQ (p.last_error (), make_error_code (error::expected_token));
