667e3a3da3a0e86aeca60ff4187330dc47f579ce
Paul Bowen-Huggett <paulhuggett@mac.com>
2023-02-01 11:23:24 +0100

    Remove dtor: draws a warning from clang.

--- unittests/test_arrayvec.cpp
+++ unittests/test_arrayvec.cpp
@@ -60,8 +60,6 @@ public:
     other.v_ = 0;
   }
 
-  ~no_copy_or_assign () noexcept = default;
-
   no_copy_or_assign &operator= (no_copy_or_assign const &) = delete;
   no_copy_or_assign &operator= (no_copy_or_assign &&other) noexcept {
     v_ = other.v_;
