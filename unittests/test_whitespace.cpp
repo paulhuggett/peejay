12da1b1086646995f0b2c6aae1c215988bf5b5bf
Paul Bowen-Huggett <paulhuggett@mac.com>
2023-02-01 08:38:44 +0100

    Use array<> rather than u32string to avoid startup allocation.
    Resolves a clang-tidy warning "initialization of 'extra_ws_chars' with
    static storage duration may throw an exception that cannot be caught".

--- unittests/test_whitespace.cpp
+++ unittests/test_whitespace.cpp
@@ -45,12 +45,10 @@ TEST_F (Whitespace, MultipleTrailingSpaces) {
 
 namespace {
 
-std::u32string const extra_ws_chars{char_set::character_tabulation,
-                                    char_set::vertical_tabulation,
-                                    char_set::space,
-                                    char_set::no_break_space,
-                                    char_set::en_quad,
-                                    '0'};
+std::array<char32_t, 6> const extra_ws_chars{
+    {char_set::character_tabulation, char_set::vertical_tabulation,
+     char_set::space, char_set::no_break_space, char_set::en_quad,
+     char_set::digit_zero}};
 
 } // end anonymous namespace
 
@@ -58,14 +56,14 @@ std::u32string const extra_ws_chars{char_set::character_tabulation,
 TEST_F (Whitespace, ExtendedWhitespaceCharactersEnabled) {
   EXPECT_CALL (callbacks_, uint64_value (0)).Times (1);
   auto p = make_parser (proxy_, extensions::extra_whitespace);
-  p.input (extra_ws_chars).eof ();
+  p.input (std::begin (extra_ws_chars), std::end (extra_ws_chars)).eof ();
   EXPECT_FALSE (p.has_error ());
 }
 
 // NOLINTNEXTLINE
 TEST_F (Whitespace, ExtendedWhitespaceCharactersDisabled) {
   auto p = make_parser (proxy_);
-  p.input (extra_ws_chars).eof ();
+  p.input (std::begin (extra_ws_chars), std::end (extra_ws_chars)).eof ();
   EXPECT_TRUE (p.has_error ());
   EXPECT_EQ (p.last_error (), make_error_code (error::expected_token));
 }
