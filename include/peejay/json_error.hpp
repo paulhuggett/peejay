9f93c42b761eb5be314ed67209ea7708238432c8
Paul Bowen-Huggett <paulhuggett@mac.com>
2023-05-14 08:59:05 +0200

    Eliminate json_error.cpp.

--- include/peejay/json_error.hpp
+++ include/peejay/json_error.hpp
@@ -16,6 +16,7 @@
 #ifndef PEEJAY_JSON_ERROR_HPP
 #define PEEJAY_JSON_ERROR_HPP
 
+#include <cassert>
 #include <string>
 #include <system_error>
 
@@ -49,24 +50,47 @@ enum class error : int {
 // ******************
 class error_category final : public std::error_category {
 public:
-  error_category () noexcept;
-  char const* name () const noexcept override;
-  std::string message (int error) const override;
+  char const* name () const noexcept override {
+    return "PJ JSON parser category";
+  }
+  std::string message (int const err) const override {
+    switch (static_cast<error> (err)) {
+    case error::none: return "none";
+    case error::bad_identifier: return "bad identifier";
+    case error::bad_unicode_code_point: return "bad UNICODE code point";
+    case error::dom_nesting_too_deep:
+      return "(DOM) object or array contains too many members";
+    case error::expected_array_member: return "expected array member";
+    case error::expected_close_quote: return "expected close quote";
+    case error::expected_colon: return "expected colon";
+    case error::expected_digits: return "expected digits";
+    case error::expected_object_member: return "expected object member";
+    case error::expected_object_key: return "expected object key";
+    case error::expected_token: return "expected token";
+    case error::invalid_escape_char: return "invalid escape character";
+    case error::invalid_hex_char: return "invalid hexadecimal escape character";
+    case error::number_out_of_range: return "number out of range";
+    case error::unexpected_extra_input: return "unexpected extra input";
+    case error::unrecognized_token: return "unrecognized token";
+    case error::nesting_too_deep: return "objects are too deeply nested";
+    case error::unterminated_multiline_comment:
+      return "unterminated /* comment";
+    case error::identifier_too_long: return "identifier too long";
+    case error::string_too_long: return "string too long";
+    }
+    assert (false && "bad error code");
+    return "unknown PJ error_category error";
+  }
 };
 
-std::error_category const& get_error_category () noexcept;
-
 inline std::error_code make_error_code (error const e) noexcept {
-  return {static_cast<int> (e), get_error_category ()};
+  static error_category const cat;
+  return {static_cast<int> (e), cat};
 }
 
 }  // end namespace peejay
 
-namespace std {
-
 template <>
-struct is_error_code_enum<peejay::error> : std::true_type {};
-
-}  // end namespace std
+struct std::is_error_code_enum<peejay::error> : std::true_type {};
 
 #endif  // PEEJAY_JSON_ERROR_HPP
