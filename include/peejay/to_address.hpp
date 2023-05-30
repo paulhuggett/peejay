5d40a080fe588b4c2b0bc4e5a8f61d9bd166d373
Paul Bowen-Huggett <paulhuggett@mac.com>
2023-05-30 22:13:08 +0200

    Write the std::to_address() wrapper correctly.

--- include/peejay/to_address.hpp
+++ include/peejay/to_address.hpp
@@ -24,12 +24,8 @@ namespace peejay {
 
 #if defined(__cpp_lib_to_address)
 template <typename T>
-constexpr T* to_address (T* p) noexcept {
-  return std::to_address (p);
-}
-template <typename T>
-constexpr auto to_address (T const& p) noexcept {
-  return std::to_address (p);
+constexpr auto to_address (T&& p) noexcept {
+  return std::to_address (std::forward<T> (p));
 }
 #else
 template <typename T>
