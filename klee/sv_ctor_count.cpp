a53235087e71689e6b48e68dc3f440d8d94afa89
Paul Bowen-Huggett <paulhuggett@mac.com>
2023-06-29 17:46:46 +0200

    Add small_vector<> ctor(count) KLEE test.

---
+++ klee/sv_ctor_count.cpp
@@ -0,0 +1,45 @@
+#include <cstddef>
+
+#ifdef KLEE_RUN
+#include <iostream>
+#include <vector>
+#endif
+
+#include <klee/klee.h>
+
+#include "av_member.hpp"
+#include "peejay/small_vector.hpp"
+#include "vcommon.hpp"
+
+int main () {
+  try {
+    constexpr std::size_t body_elements = 5;
+    constexpr std::size_t max_elements = 13;
+
+    MAKE_SYMBOLIC (member::throw_number);
+
+    peejay::small_vector<member, body_elements>::size_type count;
+    MAKE_SYMBOLIC (count);
+    klee_assume (count <= max_elements);
+
+    peejay::small_vector<member, body_elements> av{count};
+
+#ifdef KLEE_RUN
+    std::vector<member> v{count};
+
+    if (!std::equal (av.begin (), av.end (), v.begin (), v.end ())) {
+      std::cerr << "** Fail!\n";
+      return EXIT_FAILURE;
+    }
+#endif  // KLEE_RUN
+  } catch (memberex const&) {
+  }
+#ifdef KLEE_RUN
+  if (auto const inst = member::instances (); inst != 0) {
+    std::cerr << "** Fail: instances = " << inst << '\n';
+    return EXIT_FAILURE;
+  }
+  std::cerr << "Pass!\n";
+#endif  // KLEE_RUN
+  return EXIT_SUCCESS;
+}
