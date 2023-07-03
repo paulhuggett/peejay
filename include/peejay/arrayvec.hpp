2dd1aefc4665d08e1c7f845b5e52f886b2c93cab
Paul Bowen-Huggett <paulhuggett@mac.com>
2023-07-03 22:06:08 +0200

    Remove pointless const qualifiers.

--- include/peejay/arrayvec.hpp
+++ include/peejay/arrayvec.hpp
@@ -45,14 +45,14 @@ class arrayvec_base {
 protected:
   template <typename SizeType, typename InputIterator,
             typename = std::enable_if_t<input_iterator<InputIterator>>>
-  static void init (pointer_based_iterator<T> begin, SizeType *const size,
+  static void init (pointer_based_iterator<T> begin, SizeType *size,
                     InputIterator first, InputIterator last);
 
   template <typename SizeType>
-  static void init (pointer_based_iterator<T> begin, SizeType *const size,
+  static void init (pointer_based_iterator<T> begin, SizeType *size,
                     SizeType count);
   template <typename SizeType>
-  static void init (pointer_based_iterator<T> begin, SizeType *const size,
+  static void init (pointer_based_iterator<T> begin, SizeType *size,
                     SizeType count, T const &value);
 
   template <bool IsMove, typename SizeType, typename SrcType>
@@ -65,11 +65,11 @@ protected:
                      pointer_based_iterator<T> last) noexcept;
 
   template <typename SizeType, typename... Args>
-  static void resize (pointer_based_iterator<T> begin, SizeType *const size,
+  static void resize (pointer_based_iterator<T> begin, SizeType *size,
                       std::size_t new_size, Args &&...args);
 
   template <typename SizeType>
-  static void assign (pointer_based_iterator<T> begin, SizeType *const size,
+  static void assign (pointer_based_iterator<T> begin, SizeType *size,
                       std::size_t count, T const &value);
 
   static void move_range (pointer_based_iterator<T> from,
