c1dab261cc352cd63538a97b079ace9bd016fa8c
Paul Bowen-Huggett <paulhuggett@mac.com>
2023-07-04 11:18:01 +0200

    Boilerplates.

--- klee/vcommon.hpp
+++ klee/vcommon.hpp
@@ -1,3 +1,18 @@
+//===- klee/vcommon.hpp -----------------------------------*- mode: C++ -*-===//
+//*                                                  *
+//* __   _____ ___  _ __ ___  _ __ ___   ___  _ __   *
+//* \ \ / / __/ _ \| '_ ` _ \| '_ ` _ \ / _ \| '_ \  *
+//*  \ V / (_| (_) | | | | | | | | | | | (_) | | | | *
+//*   \_/ \___\___/|_| |_| |_|_| |_| |_|\___/|_| |_| *
+//*                                                  *
+//===----------------------------------------------------------------------===//
+//
+// Distributed under the Apache License v2.0.
+// See https://github.com/paulhuggett/peejay/blob/main/LICENSE.TXT
+// for license information.
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 #ifndef VCOMMON_HPP
 #define VCOMMON_HPP
 
