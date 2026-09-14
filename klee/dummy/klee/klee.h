6c59b4fab6acc92ac7b311c36636730d7b7074de
Paul Bowen-Huggett <paulhuggett@mac.com>
2026-09-14 21:47:19 +0200

    Add missing include file.

---
+++ klee/dummy/klee/klee.h
@@ -0,0 +1,39 @@
+//===- klee/dummy/klee/klee.h -----------------------------*- mode: C++ -*-===//
+//*  _   _          *
+//* | |_| |___ ___  *
+//* | / / / -_) -_) *
+//* |_\_\_\___\___| *
+//*                 *
+//===----------------------------------------------------------------------===//
+// Copyright © 2025 Paul Bowen-Huggett
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// “Software”), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+// SPDX-License-Identifier: MIT
+//===----------------------------------------------------------------------===//
+#ifndef PEEJAY_DUMMY_KLEE_HPP
+#define PEEJAY_DUMMY_KLEE_HPP
+
+#include <cstdint>
+
+void klee_make_symbolic(void *addr, size_t nbytes, char const *name);
+void klee_assume(uintptr_t x);
+
+#endif // PEEJAY_DUMMY_KLEE_HPP
